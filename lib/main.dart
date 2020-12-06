3fa36365210b297e303dd50b4c14b1420cab96cb
nporoykov <nporoykov@gmail.com>
2020-12-06 18:54:15 +0300

    3rd commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 //import 'dart:html';
 
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,6 +11,18 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    int count = 0;
+
+    void answerQuestion() {
+      ++count;
+      print('This is your answer count = ' + count.toString());
+    }
+
+    List<String> questions = [
+      'What\'s your favorite book?',
+      'What\'s your favorite fish?'
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -18,18 +32,18 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: <Widget>[
-            Text('This is the question....'),
+            Text(questions[1]),
             RaisedButton(
               child: Text('Answer 1'),
-              onPressed: null,
+              onPressed: answerQuestion,
             ),
             RaisedButton(
               child: Text('Answer 2'),
-              onPressed: null,
+              onPressed: answerQuestion,
             ),
             RaisedButton(
               child: Text('Answer 3'),
-              onPressed: null,
+              onPressed: () => print('answer 3 chosen'),
             )
           ],
         ),
